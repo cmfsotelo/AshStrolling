b0151ad240d01a12dbc74eaa2ee65cd8ecca1d25
csotelo <carlos.sotelo7@gmail.com>
2018-04-28 20:09:03 +0100

    Adding link to about Fragment

--- app/src/main/java/com/example/csotelo/ashstrolling/AboutFragment.kt
+++ app/src/main/java/com/example/csotelo/ashstrolling/AboutFragment.kt
@@ -1,14 +1,24 @@
 package com.example.csotelo.ashstrolling
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import butterknife.ButterKnife
+import butterknife.OnClick
 
 
 class AboutFragment : Fragment() {
+
+    @OnClick(R.id.tv_author)
+    fun linkClick() {
+        val uri = Uri.parse(getString(R.string.author_url))
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
